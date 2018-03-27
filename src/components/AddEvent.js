a3e88f8bb9468d0d3736bb0f43bcf00175d384f8
eliza29 <elizaseguraf@gmail.com>
2018-03-27 15:30:18 -0500

    añadir archivo addEvent

---
+++ src/components/AddEvent.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const AddEvent = () => (
+    <button className="btn btn-danger my-2 my-sm-0" type="submit">Crea un evento</button>
+)
+
+export default AddEvent
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import AddEvent from './AddEvent';
 
 const Header = () => (
   <div>
-    <nav className="navbar navbar-dark bg-secondary">
-      <span className="navbar-brand h1">Eventos</span>
+    <nav className="navbar navbar-light bg-light">
+      <a className="navbar-brand" href="">
+        <img src="https://user-images.githubusercontent.com/32284212/37991844-dd31921e-31cf-11e8-9ac2-c2966feba60b.png" width="30" height="30" alt="logo"/>
+     </a>
+     <form className="form-inline my-2 my-lg-0">
+      <AddEvent/>
+      <button className="btn btn-link my-2 my-sm-0" type="submit">Inicia Sesión</button>
+      <button className="btn btn-link my-2 my-sm-0" type="submit">Registrate</button>
+    </form>
     </nav>
-    <div class="text-center p-3 mb-2 bg-light text-dark">CREA TU EVENTO</div>
   </div>
-)
-
+  
+    )
+    
 export default Header
