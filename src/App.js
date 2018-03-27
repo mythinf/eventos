0f109ccdef7fe37d359abb0b1186b4ae3f6e0662
JackelineGCP <gessenia_17@hotmail.com>
2018-03-27 10:06:34 -0500

    Agrega inicio de sesión con cuenta de google

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
+import firebase from 'firebase';
 
-const App = () => (
-  <div>-</div>
-)
+import './App.css';
+
+class App extends Component {
+  handleAuth() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    firebase.auth().signInWithPopup(provider)
+      .then(result => console.log(`${result.user.email} ha iniciado sesión`))
+      .catch(error => console.log(`Error ${error.code}: ${error.message}`));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>Eventos</h2>
+        </div>
+        <p className="App-intro">
+          <button onClick={this.handleAuth}>Login con google</button>
+        </p>
+      </div>
+    );
+  }
+}
 
 export default App;
+
--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import firebase from 'firebase';
+
 import App from './App';
+import './index.css';
+
+  firebase.initializeApp({
+    apiKey: "AIzaSyAF9CnCrkPu9qCrnoS0sq-kXajrR-gipOo",
+    authDomain: "eventos-7f7c6.firebaseapp.com",
+    databaseURL: "https://eventos-7f7c6.firebaseio.com",
+    projectId: "eventos-7f7c6",
+    storageBucket: "",
+    messagingSenderId: "795388946771"
+  }); 
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
