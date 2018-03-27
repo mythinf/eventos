66018c097debaccd5bef4894f4a2e6c4c851362b
JackelineGCP <gessenia_17@hotmail.com>
2018-03-27 10:33:37 -0500

    funcionalidad de login y logout con firebase

--- src/App.js
+++ src/App.js
@@ -4,12 +4,54 @@ import firebase from 'firebase';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      user: null
+    };
+
+    this.handleAuth = this.handleAuth.bind(this);
+    this.handleLogout = this.handleLogout.bind(this);
+  }
+
+  componentWillMount() {
+    firebase.auth().onAuthStateChanged(user => {
+      this.setState({ user });
+    });
+  }
+
+
   handleAuth() {
     const provider = new firebase.auth.GoogleAuthProvider();
 
     firebase.auth().signInWithPopup(provider)
       .then(result => console.log(`${result.user.email} ha iniciado sesión`))
       .catch(error => console.log(`Error ${error.code}: ${error.message}`));
+  }
+
+  handleLogout () {
+    firebase.auth().signOut()
+    .then(result => console.log(`${result.user.email} ha salido`))
+    .catch(error => console.log(`Error ${error.code}: ${error.message}`));
+  }
+
+
+  renderLoginButton() {
+    // si el usuario está logeado
+    if (this.state.user) {
+      return (
+        <div>
+          <img width="100" src={this.state.user.photoURL} alt={this.state.user.displayName} />
+          <p>hola {this.state.user.displayName}!</p>
+          <button onClick={this.handleAuth}>Salir</button>
+        </div>
+      );
+    } else {
+      return(
+        // si no lo está
+      <button onClick={this.handleAuth}>Login con google</button>
+      );      
+    }
   }
 
   render() {
@@ -19,7 +61,7 @@ class App extends Component {
           <h2>Eventos</h2>
         </div>
         <p className="App-intro">
-          <button onClick={this.handleAuth}>Login con google</button>
+          {this.renderLoginButton()}
         </p>
       </div>
     );
