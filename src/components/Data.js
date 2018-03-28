2090aaf4ed7a038e552a899e3eb700eb16865dfb
milagrospalma <mnpalma.2@gmail.com>
2018-03-27 23:22:41 -0500

    agregando Data.js

---
+++ src/components/Data.js
@@ -0,0 +1,34 @@
+const data = {
+  'Arte y cultura': [
+    {
+      name: 'Técnicas básicas de grabado y linografía',
+      description: 'El grabado se basa en el concepto de transferir una imagen grabada en una matriz...',
+      image: '',
+      total: 30,
+      point: 'UNMSM',
+      date1: '12-05-2018',
+      date2: '13-05-2018',
+      time: '05:00 p.m.',
+    },
+    {
+      name: 'ENERGÍAS SOCIALES / FUERZAS VITALES. NATALIA IGUIÑIZ: ARTE, ACTIVISMO, FEMINISMO',
+      description: 'Esta exposición antológica, bajo la curaduría de Miguel A. López, reúne un conjunto amplio de pinturas, fotografías, instalaciones, afiches, objetos, collage y videos realizados por la artista peruana Natalia Iguiñiz desde 1994. Su trabajo artístico en diálogo con el feminismo ha participado activamente del debate público y las luchas por los derechos de las mujeres. Además, su obra hurga en la memoria de la violencia política y cuestiona las representaciones sexistas de los medios de comunicación, la invisibilización de la violencia doméstica y las formas de control gubernamental o religioso sobre el cuerpo de las mujeres.',
+      image: 'http://www.enlima.pe/sites/default/files/styles/imagen_masonry/public/natalia_iguiniz_afiche-perrahabla_0.jpg',
+      total: 50,
+      point: 'ICPNA Miraflores',
+      date1: '08-04-2018',
+      date2: '10-05-2018',
+      time: '11:00 a.m.',
+    },
+    {
+      name: '3D - IMPRIMIR EL MUNDO',
+      description: 'Esta exposición explora el impacto de la impresión en tres dimensiones en nuestra sociedad desde disciplinas tan variadas como la medicina, la moda, la gastronomía o la arquitectura. Una transformación que permite al ciudadano fabricar cualquier cosa, reformulando conceptos fundamentales como la autoría, la sostenibilidad, la educación o la accesibilidad. Para la presente exposición se ha organizado en cuatro grandes bloques temáticos: Del bit al átomo, Por el espejo retrovisor, La huella tridimensional y Un paso más allá.',
+      image: 'http://www.enlima.pe/sites/default/files/styles/imagen_masonry/public/nefertiti-difusion.jpg',
+      total: 100,
+      point: 'Espacio Fundación Telefónica',
+      date1: '01-04-2018',
+      date2: '15-04-2018',
+      time: '10:00 a.m.',
+    }
+  ]
+}
